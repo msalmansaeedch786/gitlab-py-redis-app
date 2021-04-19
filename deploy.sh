2e86a9bd1fe5564fa7fcb8ff221ca9160ef91a20
Muhammad Salman <muhammad.salman@empglabs.com>
2021-04-20 02:38:16 +0500

    modified code

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,7 @@
 
 ssh -o StrictHostKeyChecking=no ubuntu@$server_ip << 'ENDSSH'
   cd /home/ubuntu/app
-  export $(cat .env-ci | xargs)
-  cat .env-ci
-  docker login -u $CI_REGISTRY_USER -p $ACCESS_TOKEN $CI_REGISTRY
-  docker pull $TORNADO_IMAGE
+  git clone https://github.com/msalmansaeedch786/gitlab-py-redis-app.git
+  cd /gitlab-py-redis-app/src
   docker-compose up -d
 ENDSSH
